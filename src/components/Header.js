5cd55b9724f317357844554717e8c1df46f7e92c
Aubrey Ross <87405979+aubreyross@users.noreply.github.com>
2023-05-05 02:54:41 +0000

    Added Meme Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 export default function Header() {
     return (
         <header className="header">
-            <img src="./images/troll-face.png" className="header--image" />
+            {/* <img src="./images/troll-face.png" className="header--image" /> */}
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Project</h4>
         </header>
---
+++ src/components/Meme.js
@@ -0,0 +1,23 @@
+
+export default function Meme() {
+    return (
+        <main>
+            <form>
+                <input 
+                    type="text"
+                    placeholder="top text"
+                    className="form--input" 
+                />
+                <input 
+                    type="text"
+                    placeholder="bottom text"
+                    className="form--input" 
+                />
+                <button 
+                    className="form--button"
+                    >                 
+                Get a new meme image 🖼</button>
+            </form>
+        </main>
+    )
+}
